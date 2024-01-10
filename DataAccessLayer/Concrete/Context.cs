ac640368709e5d5c9777068e6922e50ace312d42
arda <ardaklc0@gmail.com>
2024-01-10 22:16:17 +0300

    Some fixes have been made. The main files were not present. They were added.

---
+++ DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,31 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=localhost\\SQLEXPRESS;database=TraversalDB;Integrated Security=True;" +
+                "Trusted_Connection=True;TrustServerCertificate=true;");
+        }
+
+        public DbSet<Contact> Contacts{ get; set; }
+        public DbSet<Destination> Destinations{ get; set; }
+        public DbSet<FirstAbout> FirstAbouts{ get; set; }
+        public DbSet<FirstFeature> FirstFeatures{ get; set; }
+        public DbSet<Guide> Guides{ get; set; }
+        public DbSet<Newsletter> Newsletters{ get; set; }
+        public DbSet<SecondAbout> SecondAbouts{ get; set; }
+        public DbSet<SecondFeature> SecondFeatures{ get; set; }
+        public DbSet<SubAbout> SubAbouts{ get; set; }
+        public DbSet<Testimonial> Testimonials{ get; set; }
+
+    }
+}
