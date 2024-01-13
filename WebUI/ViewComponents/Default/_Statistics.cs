5ba948f50412f639a9de9777236e01ac24682d13
arda <ardaklc0@gmail.com>
2024-01-13 12:29:40 +0300

    Features and Statistics pages have been added.

---
+++ WebUI/ViewComponents/Default/_Statistics.cs
@@ -0,0 +1,17 @@
+﻿using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.ViewComponents.Default
+{
+    public class _Statistics : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            using var c = new Context();
+            ViewBag.destinations = c.Destinations.Count();
+            ViewBag.guides = c.Guides.Count();
+            ViewBag.users = "285";
+            return View();
+        }
+    }
+}
