3e1da5d42858a309f4ea7bb5c4079149557d8cf9
arda <ardaklc0@gmail.com>
2024-06-02 22:11:35 +0300

    CommentController is added. AddComment method is added and the corresponding PartialView is added.

---
+++ WebUI/Controllers/CommentController.cs
@@ -0,0 +1,26 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.Controllers
+{
+    public class CommentController : Controller
+    {
+        CommentManager commentManager = new CommentManager(new EFCommentDal());
+
+        [HttpGet]
+        public PartialViewResult AddComment()
+        {
+            return PartialView();
+        }
+
+        [HttpPost]
+        public IActionResult AddComment(EntityLayer.Concrete.Comment comment)
+        {
+            comment.CommentDate = DateTime.Now;
+            comment.CommentState = true;
+            commentManager.TAdd(comment);
+            return RedirectToAction("Index", "Destination");
+        }
+    }
+}
