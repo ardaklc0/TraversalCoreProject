3c117bc7effe995f6b93cf3e30cf5de900d3a0bc
arda <ardaklc0@gmail.com>
2024-06-03 18:57:58 +0300

    Register and Login screens are added. Identity is added. AppRole and AppUser is added.

---
+++ WebUI/Controllers/LoginController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.Controllers
+{
+    [AllowAnonymous]
+    public class LoginController : Controller
+    {
+        [HttpGet]
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+        //[HttpPost]
+        //public IActionResult SignUp()
+        //{
+        //    return RedirectToAction("Index", "Destination");
+        //}
+
+        [HttpGet]
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+
+        //[HttpPost]
+        //public IActionResult SignIn()
+        //{
+        //    return RedirectToAction("Index", "Destination");
+        //}
+    }
+}
