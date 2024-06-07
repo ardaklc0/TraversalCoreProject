26e34a83da34c8b44e1f9c7e7711daa7af1ef2cc
arda <ardaklc0@gmail.com>
2024-06-07 19:14:16 +0300

    Profile edit section is added. LoginController is changed.

--- WebUI/Areas/Member/Controllers/ProfileController.cs
+++ WebUI/Areas/Member/Controllers/ProfileController.cs
@@ -1,12 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using TraversalCoreProject.Areas.Member.Models;
 
 namespace TraversalCoreProject.Areas.Member.Controllers
 {
+    [Area("Member")]
+    [Route("Member/[controller]/[action]")]
     public class ProfileController : Controller
     {
-        public IActionResult Index()
+        private readonly UserManager<AppUser> _userManager;
+        public ProfileController(UserManager<AppUser> userManager)
         {
-            return View();
+            _userManager = userManager;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var values = await _userManager.FindByNameAsync(User.Identity.Name);
+            UserEditViewModel model = new UserEditViewModel
+            {
+                Name = values.Name,
+                Surname = values.Surname,
+                Mail = values.Email,
+                PhoneNumber = values.PhoneNumber,
+            };
+            return View(model);
         }
     }
 }
