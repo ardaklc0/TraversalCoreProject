b40efc923ae8310f6d0b6b41284de63bb35d68b2
arda <ardaklc0@gmail.com>
2024-06-12 17:13:46 +0300

    GuideManager has been added. Corresponding ViewComponents and Component cshtml are added. Dashboard is modified (it calls InvokeAsync("_GuideList")).

---
+++ WebUI/ViewComponents/MemberDashboard/_GuideList.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.ViewComponents.MemberDashboard
+{
+    
+    public class _GuideList : ViewComponent
+    {
+        GuideManager GuideManager = new GuideManager(new EFGuideDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = GuideManager.TGetList();
+            return View(values);
+        }
+    }
+}
