3f895fdc4ded3e77ebd4501744b3b9b53ff103fe
arda <ardaklc0@gmail.com>
2024-06-14 20:43:58 +0300

    AdminController for PartialViews are added. A new controller with PartialViews in it is added. Here is the answer to very important question: // Neden bir yerde Controller kullanmadan direkt PartialView kullandık da şimdi Controller ile kullanıyoruz? // Senaryolar: // Statik İçerikler: Navbar, footer gibi sabit ve veri gerektirmeyen içerikler için Razor view içinde partial view kullanımı daha uygundur. // Dinamik İçerikler: Kullanıcı bilgisi, bildirimler, kullanıcıya özel içerikler gibi dinamik ve veri gerektiren içerikler için Controller action'ları kullanmak daha iyidir. // Bu, verilerin Controller tarafından hazırlanıp partial view'e geçirilmesine olanak tanır.

--- WebUI/Controllers/AdminController.cs
+++ WebUI/Controllers/AdminController.cs
@@ -14,5 +14,30 @@ namespace TraversalCoreProject.Controllers
             return PartialView();
         }
 
+        public PartialViewResult PartialSideBar()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialNavbar()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialFooter()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialScript()
+        {
+            return PartialView();
+        }
     }
 }
+
+// Neden bir yerde Controller kullanmadan direkt PartialView kullandık da şimdi Controller ile kullanıyoruz?
+// Senaryolar:
+// Statik İçerikler: Navbar, footer gibi sabit ve veri gerektirmeyen içerikler için Razor view içinde partial view kullanımı daha uygundur.
+// Dinamik İçerikler: Kullanıcı bilgisi, bildirimler, kullanıcıya özel içerikler gibi dinamik ve veri gerektiren içerikler için Controller action'ları kullanmak daha iyidir.
+// Bu, verilerin Controller tarafından hazırlanıp partial view'e geçirilmesine olanak tanır.
