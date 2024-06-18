a4bc19be75d72752f078829859d1fcbc12c560ae
arda <ardaklc0@gmail.com>
2024-06-18 11:04:10 +0300

    Validator for Guide is added. GuideController is changed. asp-validation is added to AddGuide View. ExcelController is added for Excel Reports. DestinationModel is added for ExcelController.

---
+++ BusinessLayer/ValidationRules/GuideValidator.cs
@@ -0,0 +1,21 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class GuideValidator : AbstractValidator<Guide>
+    {
+        public GuideValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Name cannot be empty");
+            RuleFor(x => x.Description).NotEmpty().WithMessage("Description cannot be empty");
+            RuleFor(x => x.Image).NotEmpty().WithMessage("Image cannot be empty");
+            RuleFor(x => x.Name).MinimumLength(10).WithMessage("Name must be at least 10 characters");
+        }
+    }
+}
--- WebUI/Areas/Admin/Controllers/GuideController.cs
+++ WebUI/Areas/Admin/Controllers/GuideController.cs
@@ -1,6 +1,8 @@
 ﻿using BusinessLayer.Abstract;
+using BusinessLayer.ValidationRules;
 using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
 
 namespace TraversalCoreProject.Areas.Admin.Controllers
 {
@@ -18,30 +20,45 @@ namespace TraversalCoreProject.Areas.Admin.Controllers
             var values = _guideService.TGetList();
             return View(values);
         }
+
         [HttpGet]
         public IActionResult AddGuide()
         {
             return View();
         }
+
         [HttpPost]
         public IActionResult AddGuide(Guide guide)
         {
             guide.Status = true;
-            _guideService.TAdd(guide);
-            return RedirectToAction("Index");
+            GuideValidator validationRules = new GuideValidator();
+            var result = validationRules.Validate(guide);
+            if (result.IsValid)
+            {
+                _guideService.TAdd(guide);
+                return RedirectToAction("Index");
+            }
+            foreach (var item in result.Errors)
+            {
+                ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+            }
+            return View();
         }
+
         [HttpGet]
         public IActionResult EditGuide(int id)
         {
             var values = _guideService.TGetById(id);
             return View(values);
         }
+
         [HttpPost]
         public IActionResult EditGuide(Guide guide)
         {
             _guideService.TUpdate(guide);
             return RedirectToAction("Index");
         }
+        
         public IActionResult ChangeToTrue(int id)
         {
             var value = _guideService.TGetById(id);
@@ -49,6 +66,7 @@ namespace TraversalCoreProject.Areas.Admin.Controllers
             _guideService.TUpdate(value);
             return RedirectToAction("Index");
         }
+
         public IActionResult ChangeToFalse(int id)
         {
             var value = _guideService.TGetById(id);
